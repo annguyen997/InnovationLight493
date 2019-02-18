a52af3146d6a5c1fb002afb0fc4ffe66f69df634
chong4 <38228375+chong4@users.noreply.github.com>
2019-02-18 17:44:51 -0500

    Add files via upload

---
+++ entercode.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title> 
+	Pragmatics Reset Page 
+</title>
+
+<link rel="stylesheet" href="http://helios.ite.gmu.edu/~chong4/IT207/pragmatics.css" type="text/css"/>
+</head>
+<body>
+
+<h2>
+	Pragmatics Password Reset System <br />
+</h2> 
+
+<hr />
+<hr />	
+
+
+<div id='pressbutton'>
+<form method="post" action="resetpassexpired.php">
+
+<h3> 
+Check your mobile device. Please enter the 4-digit code received into the box below. 
+</h3>
+</body>
+<table>
+<tr> 
+	<td><center>First Digit</center></td>
+	<td><center>Second Digit </center></td>
+	<td><center>Third Digit </center></td>
+	<td><center>Fourth Digit </center></td>
+</tr>
+
+<tr>
+<td height="100" width = "20"> 
+<center><select name="num1" size="10"> 
+		<option value="0">0</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		</select> </td>
+		</center> 
+
+<td height="100" width = "20"> 
+<center><select name="num2" size="10"> 
+		<option value="0">0</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		</select> </td>
+		</center>
+		
+<td height="100" width = "20"> 
+<center><select name="num3" size="10"> 
+		<option value="0">0</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		</select> </td>
+		</center>
+		
+<td height="100" width = "20"> 
+<center><select name="num4" size="10"> 
+		<option value="0">0</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		</select> </td>
+		</center>
+		
+</tr>
+
+</table>
+<body>
+<br /><br />
+<center><input type="submit" value="SUBMIT CODE" class="button"/></center>
+
+</form>
+
+</div>
+<div id='sidebar'>
+<br /><a href="http://www.pragmatics.com/">Pragmatics Home</a> <br /><br /><a href="http://helios.vse.gmu.edu/~chong4/IT207/">Password Reset Home</a> <br /><br />  Help/FAQ <br /><br /> <img src="logo.jpg" alt="logo"> <br /> <img src="IL.jpg" alt="Innovation Light">
+</div>
+
+
+</body>
+</html>
