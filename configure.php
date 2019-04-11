32f275d841599c5415a78933e92775f5a4aff9d6
An Nguyen <32471775+annguyen997@users.noreply.github.com>
2019-04-11 00:24:59 -0400

    Update configure.php

--- configure.php
+++ configure.php
@@ -3,12 +3,12 @@
     $username = 'InnovationLight@dbinnovationlight';
     $password1 = '1nnovationLight';
     $db_name = 'testdata';
-
+    
 //Establishes the connection//
 	$conn = mysqli_init();
 	mysqli_real_connect($conn, $host, $username, $password1, $db_name, 3306);
 	if (mysqli_connect_errno($conn)) {
 	die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
-	echo "connected successfully!\n";
- ?>
+	echo "Connected successfully!\n";
+?>
